fc6155c0a470f4b5a8cde2581ba82fa555f2222a
HenryMiyawaki <miyawakihenry@gmail.com>
2023-06-09 23:31:05 -0300

    add mongoContext to centralize collections

---
+++ ProcessFlow/DataAccess/MongoContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using ProcessFlow.Models;
+
+namespace ProcessFlow.DataAccess
+{
+    public class MongoContext
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoContext(IOptions<ProcessDatabaseSettings> options)
+        {
+            var mongoClient = new MongoClient(options.Value.ConnectionString);
+            _database = mongoClient.GetDatabase(options.Value.DatabaseName);
+        }
+
+        public IMongoCollection<AreaModel> AreaCollection =>
+            _database.GetCollection<AreaModel>("Area");
+    }
+}
