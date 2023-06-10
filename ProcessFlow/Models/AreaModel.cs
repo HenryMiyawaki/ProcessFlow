75df0cea52c0d0de325a0adf46d072f31599c925
HenryMiyawaki <miyawakihenry@gmail.com>
2023-06-10 04:42:01 -0300

    add description

--- ProcessFlow/Models/AreaModel.cs
+++ ProcessFlow/Models/AreaModel.cs
@@ -8,5 +8,6 @@ namespace ProcessFlow.Models
         [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
         public string? Id { get; set; }
         public string Name { get; set; }
+        public string? Description { get; set; }
     }
 }
