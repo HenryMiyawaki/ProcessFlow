a3a4d2549998931258bfcbcb3c9c3dd46de330dd
HenryMiyawaki <miyawakihenry@gmail.com>
2023-06-13 02:37:04 -0300

    adding errors handler

---
+++ ProcessFlow/Controllers/ErrorHandler/CustomExceptionFilterAttribute.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ProcessFlow.Controllers.ErrorHandler
+{
+    public class CustomExceptionFilterAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            if (context.Exception is NotFoundException)
+            {
+                context.Result = new NotFoundObjectResult(context.Exception.Message);
+                context.ExceptionHandled = true;
+            }
+            else if (context.Exception is BadRequestException)
+            {
+                context.Result = new BadRequestObjectResult(context.Exception.Message);
+                context.ExceptionHandled = true;
+            }
+
+            base.OnException(context);
+        }
+    }
+}
---
+++ ProcessFlow/Controllers/ErrorHandler/NotFoundException .cs
@@ -0,0 +1,9 @@
+﻿namespace ProcessFlow.Controllers.ErrorHandler
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message)
+        {
+        }
+    }
+}
