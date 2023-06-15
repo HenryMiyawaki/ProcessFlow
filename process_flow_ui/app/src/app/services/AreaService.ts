dcaa0a5d3b014cc394c3d504ca4a4f3800425573
HenryMiyawaki <miyawakihenry@gmail.com>
2023-06-15 06:51:50 -0300

    create areaService

--- process_flow_ui/app/src/app/services/AreaService.ts
+++ process_flow_ui/app/src/app/services/AreaService.ts
@@ -1,37 +1,32 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
-import { AreaModel } from "../models/AreaModel";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { AreaModel } from '../models/AreaModel';
 
 @Injectable({
-    providedIn: "root"
+  providedIn: 'root',
 })
-
 export class AreaService {
-    constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
-    getAreas() : any {
-        // return this.http.get<AreaModel>();
-        return [
-            {
-                id: "1",
-                name: "Area 1",
-                description: "desc",
-                processes: [{
-                    id: "1",
-                    name: "Processo 1"
-                }]
-            },
-            {
-                id: "2",
-                name: "Area 2",
-                description: "desc",
-            },
-            {
-                id: "3",
-                name: "Area 3",
-                description: "desc",
-            }
-        ]
-    }
+  getAreas(): Observable<AreaModel[]> {
+    return this.http.get<AreaModel[]>(`${environment.apiUrl}`);
+  }
+
+  getAreaById(id: string): Observable<AreaModel> {
+    return this.http.get<AreaModel>(`${environment.apiUrl}/${id}`);
+  }
+
+  createArea(area: AreaModel): Observable<AreaModel> {
+    return this.http.post<AreaModel>(`${environment.apiUrl}`, area);
+  }
+
+  updateArea(id: string, area: AreaModel): Observable<AreaModel> {
+    return this.http.put<AreaModel>(`${environment.apiUrl}/${id}`, area);
+  }
+
+  deleteArea(id: string): Observable<void> {
+    return this.http.delete<void>(`${environment.apiUrl}/${id}`);
+  }
 }
