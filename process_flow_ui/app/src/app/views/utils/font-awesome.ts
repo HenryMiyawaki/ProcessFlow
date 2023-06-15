defad9ef6be554700f049558f6a8667b753371ba
HenryMiyawaki <miyawakihenry@gmail.com>
2023-06-15 06:52:30 -0300

    add icons

--- process_flow_ui/app/src/app/views/utils/font-awesome.ts
+++ process_flow_ui/app/src/app/views/utils/font-awesome.ts
@@ -1,8 +1,16 @@
-import { faTools, faTrash, faAdd } from "@fortawesome/free-solid-svg-icons";
+import {
+  faAdd,
+  faCircleXmark,
+  faLayerGroup,
+  faTools,
+  faTrash,
+} from '@fortawesome/free-solid-svg-icons';
 
 export class FontAwesome {
-    faTools = faTools;
-    faTrash = faTrash;
-    faAdd = faAdd;
-    constructor(){}
+  faTools = faTools;
+  faTrash = faTrash;
+  faAdd = faAdd;
+  faLayerGroup = faLayerGroup;
+  faCircleXmark = faCircleXmark;
+  constructor() {}
 }
